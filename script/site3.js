69ee682db26c2394f3164befa591ae29065ac897
gustavo <ovejero.gustavo92@gmail.com>
2022-12-13 19:12:05 -0600

    Proyecto terminado

--- script/site3.js
+++ script/site3.js
@@ -8,34 +8,39 @@ else {
 }
 
 class Producto {
-    constructor(nombre, color, talle, disponible) {
+    constructor(nombre, color, talle, precio) {
         this.nombre = nombre;
         this.color = color;
         this.talle = talle;
-        this.disponible = disponible;
+        this.precio = precio;
     }
 } 
 const productos = [];
-productos.push(new Producto("ATHENEA", "Rojo" , 40, true));
-productos.push(new Producto("AURORA", "Negro" , 39, true));
-productos.push(new Producto("GAIA", "Rosa" , 38, true));
-productos.push(new Producto("DELIA", "Gris" , 37, false));
-productos.push(new Producto("CIRSE", "Blanco" , 36, false));
+productos.push(new Producto("ATHENEA", "Rojo" , 40, '100000'));
+productos.push(new Producto("AURORA", "Negro" , 39, '130000'));
+productos.push(new Producto("GAIA", "Rosa" , 38, '120000'));
+productos.push(new Producto("DELIA", "Gris" , 37, '110000'));
+productos.push(new Producto("CIRSE", "Blanco" , 36, '150000'));
 console.log(productos);
 
 
-
+let cantidad;
 function buscarProducto(nombre, talle){
     return nombre.find(objeto => objeto.nombre === talle.toUpperCase());
 }
-for (let index = 0; index < 3; index++) {
-productos.push(new Producto("GAIA", "Rosa" , 38, true));
 let busqueda = buscarProducto(productos, prompt('INGRESAR NOMBRE DEL ZAPATO \n\n 1- ATHENEA \n\n 2- AURORA \n\n 3- DELIA \n\n 4- GAIA'));
     if(busqueda != undefined){
-        alert('nombre '+ busqueda.nombre+' color '+ busqueda.color+' talle '+ busqueda.talle);
+    cantidad =prompt('nombre '+ busqueda.nombre +' \n\ncolor '+ busqueda.color+' \n\ntalle '+ busqueda.talle + '\n\nprecio:' + busqueda.precio + " \n\nElegi la cantidad del producto...");
     }else{
         alert('NO EXISTE ZAPATO CON ESE NOMBRE');
     }
-}
+
 console.log(buscarProducto);
+console.log(cantidad * busqueda.precio);
+
+
+let correo = prompt("El precio total es " + cantidad * busqueda.precio + " \nLe mandaremos un mail con el detalle de la compra, por favor introduzca su correo electronico");
+alert("Se enviara el detalle al siguiente correo: " + correo + " \nGracias por realizar su compra");
+
+
 
